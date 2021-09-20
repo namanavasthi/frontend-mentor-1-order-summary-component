e42da2d1f4c96107cdd81b5ba79d964f3dc803df
Naman Avasthi <naman.avasthi@gmail.com>
2021-09-20 09:48:37 -0700

    modifications

--- src/Music.jsx
+++ src/Music.jsx
@@ -5,13 +5,13 @@ export const Music = () => {
   return (
     <article className="bg-neutral-pale rounded-xl p-4 mt-5 flex flex-row text-sm">
       <img src={icon} alt="music" />
-      <article className="flex flex-row justify-between flex-1">
-        <section className="flex flex-col pl-4 justify-around">
+      <section className="flex flex-row justify-between flex-1">
+        <header className="flex flex-col pl-4 justify-around">
           <h2 className="text-neutral-dark font-bold">Annual Plan</h2>
           <span className="text-neutral-desaurated">$59.99/year</span>
-        </section>
+        </header>
         <button className="text-primary-bright underline focus:opacity-60">Change</button>
-      </article>
+      </section>
     </article>
   );
 };
