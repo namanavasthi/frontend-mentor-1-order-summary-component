819fd1011bb82d0392212385e59d0d2e61528639
Naman Avasthi <naman.avasthi@gmail.com>
2021-09-20 09:39:46 -0700

    music icon -> music alt

--- src/Music.jsx
+++ src/Music.jsx
@@ -4,7 +4,7 @@ import icon from "./images/icon-music.svg";
 export const Music = () => {
   return (
     <article className="bg-neutral-pale rounded-xl p-4 mt-5 flex flex-row text-sm">
-      <img src={icon} alt="music icon" />
+      <img src={icon} alt="music" />
       <article className="flex flex-row justify-between flex-1">
         <section className="flex flex-col pl-4 justify-around">
           <h2 className="text-neutral-dark font-bold">Annual Plan</h2>
